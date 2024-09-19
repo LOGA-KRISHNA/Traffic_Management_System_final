9118c5352a1ffb84e0c10503b1505123eeb5a30c
LOGA-KRISHNA <130237929+LOGA-KRISHNA@users.noreply.github.com>
2024-09-19 17:17:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,3 @@
-# import math
-# from ultralytics import YOLO
-# import cv2
-# import cvzone
-# from sort import *
-# # cap=cv2.VideoCapture(0) =>Web cam
-# # cap.set(3,1280) => these two are size of the camera
-# # cap.set(4,720)
-# cap=cv2.VideoCapture('Videos/vedio.mp4')
-# #D:\MachineLearning\Project1\SIH\main.py
-# model = YOLO('best.pt')
-# className = ['ambulance', 'autorickshaw', 'bicycle', 'bus', 'car', 'emergency', 'motorcycler', 'truck']
-# print(len(className))
-# mask=cv2.imread("Videos/Untitled design (1).png")
-# tracker=Sort(max_age=20,min_hits=2,iou_threshold=0.3)
-# while True:
-#     success,img=cap.read()
-#    # imgRegion=cv2.bitwise_and(img,mask)
-#     results=model(img,stream=True)
-#     for r in results:
-#         boxes=r.boxes
-#         for box in boxes:
-#            x1,y1,x2,y2=box.xyxy[0]
-#            x1,y1,x2,y2=int(x1),int(y1),int(x2),int(y2)
-#            w,h=x2-x1,y2-y1
-#            cvzone.cornerRect(img,(x1,y1,w,h))
-#            conf=box.conf[0]
-#            conf=math.ceil(conf*100)/100
-#            cls=int(box.cls[0])
-#            #currclass=className[cls]
-#
-#            cvzone.putTextRect(img,f' {conf}',(max(0,x1),max(0,y1)),
-#                               thickness=1,scale=0.6,offset=3)
-#
-#     cv2.imshow("Image",img)
-#    # cv2.imshow("image",imgRegion)
-#     cv2.waitKey(1)
-
 
 import cv2
 from collections import defaultdict
@@ -108,3 +70,42 @@ with sv.VideoSink("new.mp4", video_info) as sink:
 
 # Release the video capture
 cap.release()
+
+# import math
+# from ultralytics import YOLO
+# import cv2
+# import cvzone
+# from sort import *
+# # cap=cv2.VideoCapture(0) =>Web cam
+# # cap.set(3,1280) => these two are size of the camera
+# # cap.set(4,720)
+# cap=cv2.VideoCapture('Videos/vedio.mp4')
+# #D:\MachineLearning\Project1\SIH\main.py
+# model = YOLO('best.pt')
+# className = ['ambulance', 'autorickshaw', 'bicycle', 'bus', 'car', 'emergency', 'motorcycler', 'truck']
+# print(len(className))
+# mask=cv2.imread("Videos/Untitled design (1).png")
+# tracker=Sort(max_age=20,min_hits=2,iou_threshold=0.3)
+# while True:
+#     success,img=cap.read()
+#    # imgRegion=cv2.bitwise_and(img,mask)
+#     results=model(img,stream=True)
+#     for r in results:
+#         boxes=r.boxes
+#         for box in boxes:
+#            x1,y1,x2,y2=box.xyxy[0]
+#            x1,y1,x2,y2=int(x1),int(y1),int(x2),int(y2)
+#            w,h=x2-x1,y2-y1
+#            cvzone.cornerRect(img,(x1,y1,w,h))
+#            conf=box.conf[0]
+#            conf=math.ceil(conf*100)/100
+#            cls=int(box.cls[0])
+#            #currclass=className[cls]
+#
+#            cvzone.putTextRect(img,f' {conf}',(max(0,x1),max(0,y1)),
+#                               thickness=1,scale=0.6,offset=3)
+#
+#     cv2.imshow("Image",img)
+#    # cv2.imshow("image",imgRegion)
+#     cv2.waitKey(1)
+
